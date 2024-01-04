001555de68d40c0d1665f86267ddffd8bc6aaafc
thegoes <guilhermegoesoc32@gmail.com>
2024-01-04 11:17:31 -0300

    Configurando-o-projeto

---
+++ src/main/java/com/generation/projetofarmacia/ProjetofarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetofarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofarmaciaApplication.class, args);
+	}
+
+}
